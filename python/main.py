a57c301c381cdc3ad66b4227e8de7df94cce2a71
James Gadoury <gadouryjames@gmail.com>
2020-12-27 19:51:26 -0500

    Add simple unit tests

--- python/main.py
+++ python/main.py
@@ -1,8 +1,29 @@
 import pathgraph
 import robotsearch
+import unittest
+
+class TestGraphMethods(unittest.TestCase):
+
+    def test_create_undirected_graph(self):
+        self.assertTrue(isinstance(pathgraph.graph_by_type("undirected"), pathgraph.UndirectedGraph))
+    
+    def test_create_directed_graph(self):
+        self.assertTrue(isinstance(pathgraph.graph_by_type("directed"), pathgraph.DirectedGraph))
+
+    def test_add_duplicate_path_undirected(self):
+        graph = pathgraph.graph_by_type("undirected")
+        path  = pathgraph.Path("B", 1)
+        self.assertTrue(graph.add_path(key="A", path=path))
+        self.assertFalse(graph.add_path(key="A", path=path))
+    
+    def test_add_duplicate_path_directed(self):
+        graph=pathgraph.graph_by_type("directed")
+        path  = pathgraph.Path("B", 1)
+        self.assertTrue(graph.add_path(key="A", path=path))
+        self.assertFalse(graph.add_path(key="A", path=path))
 
 def main():
-    print("Shortest path from Start:", ",".join(robotsearch.dijkstra_search(graph = pathgraph.dumb_graph("directed"), start = "Start", target = "End")))
+    unittest.main()
 
 if __name__ == "__main__":
     main()
